99b65f40063be389bb3efd63aa21f6fd7d72c8a3
davidonlinearchive <davidonlinearchive@gmail.com>
2026-02-21 15:40:41 -0500

    add Disable-KerbPreAuth

--- Attacks/AS-REP-Roasting/Disable-KerbPreAuth.ps1
+++ Attacks/AS-REP-Roasting/Disable-KerbPreAuth.ps1
@@ -1,1 +1,17 @@
-# This Script disables 2 Domain users kerberos pre authentication, pick at random under the LabUsers OU
+# DESCRIPTION: Disables Kerberos Pre-Authentication for 2 randomly selected users in the "LabUsers" OU
+# DOMAIN: doa.local
+
+
+Import-Module ActiveDirectory
+
+# Get all users from the LabUsers OU
+$LabUsers = Get-ADUser -Filter * -SearchBase "OU=LabUsers,DC=doa,DC=local" -Properties UserAccountControl
+
+# Pick 2 random users
+$TargetUsers = $LabUsers | Get-Random -Count 2
+
+
+foreach ($User in $TargetUsers) {
+    Set-ADAccountControl -Identity $User -DoesNotRequirePreAuth $true
+    Write-Host "Disabled Kerberos Pre-Auth for: $($User.SamAccountName)"
+}
